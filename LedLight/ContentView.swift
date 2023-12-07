1a209b98a857ac269c60da71730c9d84752bc4cc
Alexandr Rodionov <alexrodionby@gmail.com>
2023-12-07 12:26:09 +0300

    added a starter project, made edits to gitignore

---
+++ LedLight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LedLight
+//
+//  Created by Alexandr Rodionov on 7.12.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LedLight/LedLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LedLightApp.swift
+//  LedLight
+//
+//  Created by Alexandr Rodionov on 7.12.23.
+//
+
+import SwiftUI
+
+@main
+struct LedLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
